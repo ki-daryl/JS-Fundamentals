c2bcaf70530ca4d294e6a6c66abfc1e648c9d9ca
Ezekiel Kielson Sackey <ezekielkielsonsackey.github.com>
2025-07-12 17:49:05 -0700

    factorial

---
+++ 10-factorial.js
@@ -0,0 +1,9 @@
+function factorial(n) {
+  if (n <= 1 || Number.isNaN(n)) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = Number(process.argv[2]);
+console.log(factorial(num));
