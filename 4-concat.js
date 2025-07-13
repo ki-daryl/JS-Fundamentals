7b5fff5387087130b910411e3077cbe16869ce39
Ezekiel Kielson Sackey <ezekielkielsonsackey.github.com>
2025-07-12 17:26:38 -0700

    concat

--- 4-concat.js
+++ 4-concat.js
@@ -1,10 +1,4 @@
-const arg1 = 'c';
-const arg2 = 'cool';
-const arg3 = process.argv[3];
 const arg4 = process.argv[2];
 const arg5 = process.argv[3];
 
-
-console.log(`${arg1} is ${arg2}`);
-console.log(`${arg1} is ${arg3}`);
 console.log(`${arg4} is ${arg5}`);
